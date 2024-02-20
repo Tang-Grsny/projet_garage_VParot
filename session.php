a169ad23d3a65a8a48144cf4223855bdcaf68377
Tang <144221925+Tang-Grsny@users.noreply.github.com>
2024-02-20 21:51:37 +0100

    Add files via upload

--- session.php
+++ session.php
@@ -16,16 +16,20 @@
 <?php require_once(__DIR__ . '/header.php'); ?>
 
 <div class="d-flex flex-column min-vh-100">
-    <h1>Connexion</h1>
-    <form method="POST" action="login_post.php">
-        <label for="email">Email:</label>
-        <input type="email" name="email" ><br>
-        <label for="password">Mot de passe:</label>
-        <input type="password" name="password" ><br>
-        <input type="submit" value="Se connecter">
-    </form>
-    <?php require_once(__DIR__ . '/footer.php'); ?>
-</div>
+        <div class="container">
+            <h1>Connexion</h1>
+            <form action="login_post.php" method="POST">
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email:</label>
+                    <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
+                <div class="mb-3">
+                    <label for="password" class="form-label">Mot de passe:</label>
+                    <input type="password" class="form-control" name="password" ><br>
+                </div>
+                <input type="submit" value="Se connecter">
+            </form>
+        </div>
+    </div>
 
 </body>
 </html>
